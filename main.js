67bcb00b1ac407132463d5511750f685b19c3888
LeoZhengZzz <zhengshl2012@163.com>
2018-04-17 10:08:53 +0800

    fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let canvas = '';
 let context = '';
 let isPaint = false;
+let lastPoint = {};
 
 class Painter {
     constructor() {
@@ -14,18 +15,42 @@ class Painter {
         canvas.width = pageWidth;
         canvas.height = pageHeight;
 
-        context.fillStyle = 'red';
-        context.fillRect(0, 0, 100, 100); // 注意这个要写在style前面，否则style那个不会生效
+        // context.strokeStyle = 'yellow';
+        // context.strokeRect(10, 10, 100, 100); // 描边
+        //
+        // context.fillStyle = 'red'; // 填充
+        // context.fillRect(10, 10, 100, 100); // 注意这个要写在style前面，否则style那个不会生效
+        //
+        // context.clearRect(50, 50, 10, 10);
+
+        // context.beginPath();
+        // context.moveTo(200, 200);
+        // context.lineTo(300, 300);
+        // context.lineTo(200, 300);
+        // context.fill(); // 颜色为最后的颜色 现在为红色
+
+        // context.arc(150, 150, 20, 0, Math.PI / 2); // 圆心 20半径 从0度 - 90度
+        // context.stroke(); // 描边
+        // context.fill(); // 全填充
+
 
         this.listenMouseEvent();
+
+        // this.drawLine(0, 0, 100, 100);
     }
 
-    drawLine(x1, y1, x2 = 100, y2 = 100) {
+    drawCircle(x, y, r) {
+        context.beginPath();
+        context.arc(x, y, r, 0, Math.PI * 2); // 圆心 20半径 从0度 - 90度
+        context.fill(); // 描边
+    }
+
+    drawLine(x1, y1, x2, y2) {
         context.beginPath();
         context.strokeStyle = 'black';
-        context.moveTo(x1, y1);
-        context.lineWidth = 5;
-        context.moveTo(x2, y2);
+        context.moveTo(x1, y1); // 起点
+        context.lineWidth = 5; // 线的宽度
+        context.lineTo(x2, y2); // 终点
         context.stroke();
         context.closePath();
     }
@@ -33,24 +58,29 @@ class Painter {
     listenMouseEvent() {
         canvas.onmousedown = (e) => {
             isPaint = true;
-            const x = e.clientX;
+            const x = e.clientX; // 这个相对于viewport的位置
             const y = e.clientY;
-            const div = document.createElement('div');
-            div.style = 'position: absolute; left:' + (x - 3) + 'px; top:' + (y - 3) + 'px;' +
-                'width: 6px; height: 6px; background: black; border-radius: 3px';
-            canvas.appendChild(div);
+            // const div = document.createElement('div');
+            // div.style = 'position: absolute; left:' + (x - 3) + 'px; top:' + (y - 3) + 'px;' +
+            //     'width: 6px; height: 6px; background: black; border-radius: 3px';
+            // canvas.appendChild(div);
+            this.drawCircle(x, y, 1);
         }
 
         canvas.onmousemove = (e) => {
+            const x = e.clientX;
+            const y = e.clientY;
             if (isPaint) {
-                const x = e.clientX;
-                const y = e.clientY;
+                this.drawCircle(x, y, 1);
 
-                const div = document.createElement('div');
-                div.style = 'position: absolute; left:' + (x - 3) + 'px; top:' + (y - 3) + 'px;' +
-                    'width: 6px; height: 6px; background: black; border-radius: 3px';
-                canvas.appendChild(div);
+                this.drawLine(lastPoint.x, lastPoint.y, x, y)
+                // const div = document.createElement('div');
+                // div.style = 'position: absolute; left:' + (x - 3) + 'px; top:' + (y - 3) + 'px;' +
+                //     'width: 6px; height: 6px; background: black; border-radius: 3px';
+                // canvas.appendChild(div);
             }
+
+            lastPoint = {x, y};
 
         }
 
